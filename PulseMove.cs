d31e5ed435bb55051417d6c772094aba3b642e1b
Aidan Kwok <121518425+AKWOK15@users.noreply.github.com>
2023-08-02 14:29:55 -0700

    Update PulseMove.cs

--- PulseMove.cs
+++ PulseMove.cs
@@ -3,19 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-
+//Moves the pulses used in FrequencyScenario.cs
 namespace VRAVE
 {
     public class PulseMove : MonoBehaviour
     {
         //How far the pulses can go in front of the victim car before they get deleted
         private float DeadZone;
-
-
-        private CarController controls;
-        //private GameObject pulseSpawner;
-        //private PulseSpawner script;
-
+        private CarController controls
         private float moveSpeed;
         private GameObject car;
         public bool isUser;
@@ -28,14 +23,9 @@ namespace VRAVE
 
         void Awake()
         {
-            //pulseSpawnerObject = GetComponent<PulseSpawner>();
-
-            //Debug.Log("pulseSpawnerObject:" + pulseSpawnerObject);
-            //script = pulseSpawnerObject.GetComponent<PulseSpawner>();
-            //isUser = script.isUser;
-            //Debug.Log("isUser:" + isUser);
             rend = GetComponent<Renderer>();
             DeadZone = 4f;
+            //Pulses vary in speed for user and victim since user's pulses need to move slower to appear as if they are going backwards while victim's pulses need to move forward
             if (isUser)
             {
                 car = GameObject.Find("Player");
@@ -43,6 +33,7 @@ namespace VRAVE
             }
             else
             {
+                //changes color of pulses to grey
                 rend.material.color = Color.grey;
                 car = GameObject.Find("UnsuspectingAIVehicle");
                 controls = car.GetComponent<CarController>();
@@ -50,13 +41,12 @@ namespace VRAVE
         }
 
 
-        // Update is called once per frame
+        // Update is called once per frame and is responsible for moving the pulses
         void Update()
         {
             moveSpeed = controls.CurrentSpeed;
             if (isUser)
             {
-                //transform.position = transform.position + (new Vector3(0f, 0f, -0.1f) * moveSpeed) * Time.deltaTime;
                 transform.position += (new Vector3(0f, 0f, 0.3f) * moveSpeed) * Time.deltaTime;
                 if (transform.position.z + DeadZone < car.transform.position.z)
                 {
@@ -66,8 +56,6 @@ namespace VRAVE
             else
             {
                 transform.position += (new Vector3(0f, 0f, 0.6f) * moveSpeed) * Time.deltaTime;
-                //transform.position = transform.position + (new Vector3(0,0,1) * moveSpeed) *Time.deltaTime;
-                //Debug.Log("transform.position"+ transform.position);
                 if (transform.position.z > car.transform.position.z + DeadZone)
                 {
                     Destroy(gameObject);
